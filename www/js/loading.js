c3fcf5369db4880c89b48d33f1c67da23801f0c9
Auliaas <septianaauliaanisa@gmail.com>
2017-03-16 14:14:40 +0700

    versi 1.0.0

---
+++ www/js/loading.js
@@ -0,0 +1,18 @@
+angular.module('ionicApp', ['ionic'])
+.controller('loadCtrl', function($timeout, $ionicLoading) {
+
+  // Setup the loader
+  $ionicLoading.show({
+    content: 'Loading',
+    animation: 'fade-in',
+    showBackdrop: true,
+    maxWidth: 200,
+    showDelay: 0
+  });
+  
+  // Set a timeout to clear loader, however you would actually call the $ionicLoading.hide(); method whenever everything is ready or loaded.
+  $timeout(function () {
+    $ionicLoading.hide();
+  }, 2000);
+  
+});
